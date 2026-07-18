2f90e83a9b23d01dc321e017ec74f71b921687ca
Heyner Avila Sibaja <77852471+HeynerAvila@users.noreply.github.com>
2026-07-18 11:41:56 -0600

    Add files via upload

--- conexion.php
+++ conexion.php
@@ -1,14 +1,19 @@
 <?php
 
-$servidor = "@mysql.railway.internal:3306";
-$usuario = "root";
-$password = "VGFRFUgEYIAsnTvUTDIppxKtLIDoJMmK";
-$bd = "railway";
+postgresql://netlifydb_owner:npg_vmY1d5kRLIAM@ep-sparkling-waterfall-ajvd64qc.c-3.us-east-2.db.netlify.com/netlifydb?channel_binding=require&sslmode=require
 
-//mysql://root:VGFRFUgEYIAsnTvUTDIppxKtLIDoJMmK@mysql.railway.internal:3306/railway
+
+$servidor = "ep-sparkling-waterfall-ajvd64qc.c-3.us-east-2.db.netlify.com";
+$usuario = "netlifydb_owner";
+$password = "npg_vmY1d5kRLIAM";
+$bd = "netlifydb";
+
+//postgresql://netlifydb_owner:npg_vmY1d5kRLIAM@ep-sparkling-waterfall-ajvd64qc.c-3.us-east-2.db.netlify.com/netlifydb?channel_binding=require&sslmode=require
+
+$conexion = new PDO("pgsql:host=$servidor;dbname=$bd", $usuario, $password);
 
 //${{ MySQL.MYSQL_URL }}
-$conexion = new mysqli($servidor, $usuario, $password, $bd);
+//$conexion = new mysqli($servidor, $usuario, $password, $bd);
 
 if($conexion->connect_error){
     die("Error de conexión: ".$conexion->connect_error);
